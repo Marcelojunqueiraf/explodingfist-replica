b950ff8b6ce153a08e1d99e8cbecdbbfa92ae6e9
Marcelo Junqueira <marcelojunqueiraf@gmail.com>
2020-12-14 23:24:17 -0300

    include de animações

--- Includes.s
+++ Includes.s
@@ -3,14 +3,55 @@
 .include "./Imagens/Fundos/fundo1.s"
 .include "./Imagens/Fundos/fundo2.s"
 .include "./Imagens/Fundos/fundo3.s"
+
+.include ".\Imagens\Sprites\Ataques\cambalhota1.s"
+.include ".\Imagens\Sprites\Ataques\cambalhota2.s"
+.include ".\Imagens\Sprites\Ataques\cambalhota3.s"
+.include ".\Imagens\Sprites\Ataques\chuteatras.s"
+.include ".\Imagens\Sprites\Ataques\chutediagonacima.s"
+.include ".\Imagens\Sprites\Ataques\chutedireitabaixo.s"
+.include ".\Imagens\Sprites\Ataques\chuteesquerdabaixo.s"
+.include ".\Imagens\Sprites\Ataques\chutegirando1.s"
+.include ".\Imagens\Sprites\Ataques\chutegirando2.s"
+.include ".\Imagens\Sprites\Ataques\chutegirando3.s"
+.include ".\Imagens\Sprites\Ataques\chutegirando4.s"
+.include ".\Imagens\Sprites\Ataques\chutegirando5.s"
+.include ".\Imagens\Sprites\Ataques\chutegirando6.s"
+.include ".\Imagens\Sprites\Ataques\chutegirando7.s"
+.include ".\Imagens\Sprites\Ataques\chuteinferior.s"
+.include ".\Imagens\Sprites\Ataques\highkick1.s"
+.include ".\Imagens\Sprites\Ataques\highkick2.s"
+.include ".\Imagens\Sprites\Ataques\jab1.s"
+.include ".\Imagens\Sprites\Ataques\jab2.s"
+.include ".\Imagens\Sprites\Ataques\midkick.s"
+.include ".\Imagens\Sprites\Ataques\prepvoltachuteatras.s"
+.include ".\Imagens\Sprites\Ataques\prepvoltchuteinferior.s"
+.include ".\Imagens\Sprites\Ataques\socoagachado.s"
+.include ".\Imagens\Sprites\Ataques\voadora1.s"
+.include ".\Imagens\Sprites\Ataques\voadora2.s"
+.include ".\Imagens\Sprites\Curvar\curvar1.s"
+.include ".\Imagens\Sprites\Curvar\curvar2.s"
+.include ".\Imagens\Sprites\defesa\defesaempe.s"
+.include ".\Imagens\Sprites\defesa\defesasentado.s"
+.include ".\Imagens\Sprites\hit\hit1.s"
+.include ".\Imagens\Sprites\hit\hit2.s"
+.include ".\Imagens\Sprites\hit\hit3.s"
+.include ".\Imagens\Sprites\hit\hit4.s"
+.include ".\Imagens\Sprites\hit\hit5.s"
+.include ".\Imagens\Sprites\hit\hit6.s"
+.include ".\Imagens\Sprites\hit\hit7.s"
+.include ".\Imagens\Sprites\hit\hit8.s"
+.include ".\Imagens\Sprites\hit\hit9.s"
+.include ".\Imagens\Sprites\hit\hit10.s"
+.include ".\Imagens\Sprites\hit\hit11.s"
+.include ".\Imagens\Sprites\miscelaneo\misc1.s"
+.include ".\Imagens\Sprites\miscelaneo\misc2.s"
+.include ".\Imagens\Sprites\Pulo\pulo1.s"
+.include ".\Imagens\Sprites\Pulo\pulo2.s"
 .include "./Imagens/Sprites/Saudacao/ola1.s"
 .include "./Imagens/Sprites/Saudacao/ola2.s"
 .include "./Imagens/Sprites/Saudacao/ola3.s"
 .include "./Imagens/Sprites/Saudacao/ola4.s"
-.include "./Imagens/Sprites/Saudacao/P2ola1.s"
-.include "./Imagens/Sprites/Saudacao/P2ola2.s"
-.include "./Imagens/Sprites/Saudacao/P2ola3.s"
-.include "./Imagens/Sprites/Saudacao/P2ola4.s"
 .include ".\Imagens\Sprites\WalkCycle\andar1.s"
 .include ".\Imagens\Sprites\WalkCycle\andar2.s"
 .include ".\Imagens\Sprites\WalkCycle\andar3.s"
@@ -18,3 +59,44 @@
 .include ".\Imagens\Sprites\WalkCycle\andar5.s"
 .include ".\Imagens\Sprites\WalkCycle\andar6.s"
 .include ".\Imagens\Sprites\WalkCycle\andar7.s"
+
+#Andar
+#andar7, andar6, andar5, andar6, andar7, andar4, andar3, andar2, andar1
+
+#High Punch (Soco Alto)
+#jab1,jab2
+
+#Jump
+#voadora2
+
+#Forward somersault (Cambalhota)
+#cambalhota1, cambalhota 2, misc1, cambalhota3
+
+#Walk Back (Mesmo do Andar)
+#andar7, andar6, andar5, andar6, andar7, andar4, andar3, andar2, andar1
+
+#Back Somersault (cambalhota para trás)
+#cambalhota3, misc1, cambalhota2, cambalhota1
+
+#Crouch (Agaixar)
+#defesasentado
+
+#jab (Soco médio) Pra gente vai ser soco baixo
+#jab1, socoacachado
+#Mid Kick (Chute Médio)
+#High Kick (Chute Alto)
+#Flying kick (Pulo com chute)
+#High back Kick (Chuto pra trás)
+#Round House (Chute giratorio)
+#Backwards sweep (Rasteira pra trás)
+#Forward sweep (Rasteira pra frente)
+#Short jab kick (Chute baixo)
+
+#Saudacao final
+#ola1, ola2, ola3, ola4
+
+
+.include "./Imagens/Sprites/Saudacao/P2ola1.s"
+.include "./Imagens/Sprites/Saudacao/P2ola2.s"
+.include "./Imagens/Sprites/Saudacao/P2ola3.s"
+.include "./Imagens/Sprites/Saudacao/P2ola4.s"
